16526208ff3e2afdf0c17d266d5c7e1d89851d7c
Levi <udaybindal2312@gmail.com>
2024-06-02 11:04:26 +0530

    nix: enable xwayland support
    * Add some comments
    * enable Xwayland (an X server for interfacing X11 apps with the Wayland protocol).

--- nixos/hyprland.nix
+++ nixos/hyprland.nix
@@ -11,13 +11,22 @@
   };
 
   # Enable hyprland and related stuff
-  programs.hyprland.enable = true;
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  # Hint Electon apps to use wayland
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  # mouse/touchpad cursor
   environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
+  # waybar
   programs.waybar.enable = true;
 
   # Some system packages
   environment.systemPackages = with pkgs; [
+    hyprland
+    waybar
     pyprland
     hyprpaper
     hyprpicker
