a28d19981694fe366367d949b4585bf79a20cc3c
Levi <udaybindal2312@gmail.com>
2024-06-05 17:06:32 +0530

    home-manager: fix iconTheme pkg path
    * icons should be in out/share/icons

--- home-manager/kora-icons.nix
+++ home-manager/kora-icons.nix
@@ -1,23 +1,19 @@
 { pkgs, ... }:
 
-  # Create a package for kora-icons
-  pkgs.stdenv.mkDerivation {
-    name = "kora-icons";
- 
-    src = pkgs.fetchFromGitHub {
-      owner = "bikass";
-      repo = "kora";
-      # url = "https://github.com/bikass/kora";
-      rev = "0f0d48f7ef98f19732ac0dcb175954d8db32a18b";
-      sha256 = "0aa8vr273xjv5c3sn18fbg6i736g9hv8dx77xa0izkzxd6a7j9dm";
-    };
+    # Create a package for kora-icons
+    pkgs.stdenv.mkDerivation {
+      name = "kora-icons";
+      src = pkgs.fetchFromGitHub {
+        owner = "bikass";
+        repo = "kora";
+        # url = "https://github.com/bikass/kora";
+        rev = "0f0d48f7ef98f19732ac0dcb175954d8db32a18b";
+        sha256 = "0aa8vr273xjv5c3sn18fbg6i736g9hv8dx77xa0izkzxd6a7j9dm";
+      };
+  
+    installPhase = ''
+      mkdir -p $out/share/icons
+      cp -R ./*k* $out/share/icons/
+    '';
 
-  dontUnpack = true;
-
-  installPhase = ''
-    mkdir -p $out
-    cp -R ./* $out/
-  '';
-
-  }
-
+    }
