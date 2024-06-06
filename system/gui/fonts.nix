8a5df68e607ede5a03d2f77c45b245bbfa4d2749
Levi <udaybindal2312@gmail.com>
2024-06-06 19:59:35 +0530

    system/gui: add more fonts

--- system/gui/fonts.nix
+++ system/gui/fonts.nix
@@ -5,6 +5,11 @@
   fonts.packages = with pkgs; [
     font-awesome # for waybar icons
     jetbrains-mono
-    nerd-font-patcher
+    noto-fonts
+    noto-fonts-patcher
+    noto-fonts-emoji
+    twemoji-color-font
+    powerline-fonts
+    powerline-symbols
   ];
 }
