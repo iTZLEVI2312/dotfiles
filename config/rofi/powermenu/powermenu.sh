a4dc81444165c1a7bc577835f0db4d9195c646b9
Levi <udaybindal2312@gmail.com>
2024-06-16 19:07:34 +0530

    config/rofi/powermenu: add hibernate option
    * also added hyprlock on suspend

--- config/rofi/powermenu/powermenu.sh
+++ config/rofi/powermenu/powermenu.sh
@@ -18,7 +18,8 @@ host=`hostname`
 shutdown='  Shutdown'
 reboot='󰜉  Reboot'
 lock='  Lock'
-suspend='󰒲  Suspend'
+suspend='󰒲  Suspend/Sleep'
+hibernate='󰂠 Hibernate'
 logout='  Logout'
 yes='  Yes'
 no='  No'
@@ -51,7 +52,7 @@ confirm_exit() {
 
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
+	echo -e "$lock\n$suspend\n$hibernate\n$logout\n$reboot\n$shutdown" | rofi_cmd
 }
 
 # Execute Command
@@ -65,18 +66,12 @@ run_cmd() {
 		elif [[ $1 == '--suspend' ]]; then
 			mpc -q pause
 			amixer set Master mute
-			systemctl suspend
+			hyprlock && systemctl suspend
+    elif [[ $1 == '--hibernate' ]]; then
+      systemctl hibernate
 		elif [[ $1 == '--logout' ]]; then
-			if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
-				openbox --exit
-			elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
-				bspc quit
-			elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
-				i3-msg exit
-			elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
-				qdbus org.kde.ksmserver /KSMServer logout 0 0 0
-			fi
-		fi
+	    hyprctl dispatch exit 0
+    fi
 	else
 		exit 0
 	fi
@@ -96,6 +91,9 @@ case ${chosen} in
         ;;
     $suspend)
 		run_cmd --suspend
+        ;;
+    $hibernate)
+    run_cmd --hibernate
         ;;
     $logout)
 		run_cmd --logout
