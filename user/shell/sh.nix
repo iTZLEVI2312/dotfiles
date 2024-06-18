8b9e5f0b06cd8d2799a8d91534f23edefa1b1a7c
Levi <udaybindal2312@gmail.com>
2024-06-18 19:16:33 +0530

    user/shell: add more aliases

--- user/shell/sh.nix
+++ user/shell/sh.nix
@@ -4,8 +4,22 @@ let
 
   # shell aliases
   shAliases = {
-    ls = "eza --icons -l -T -L=1";
-    ll = "ls -l";
+
+    # Helpful aliases
+    c = "clear"; # clear terminal
+    l = "eza -lh  --icons=auto"; # long list
+    ls = "eza -1   --icons=auto"; # short list
+    ll = "eza -lha --icons=auto --sort=name --group-directories-first"; # long list all
+    ld = "eza -lhD --icons=auto"; # long list dirs
+
+    # Handy change dir shortcuts
+    ".." = "cd ..";
+    "..." = "cd ../..";
+    ".3" = "cd ../../..";
+    ".4" = "cd ../../../..";
+    ".5" = "cd ../../../../..";
+
+    # others
     flake-update = "nix flake update";
     nix-switch = "sudo nixos-rebuild switch --flake ${userConfig.dotfilesDir}";
     home-switch = "home-manager switch --flake ${userConfig.dotfilesDir}";
